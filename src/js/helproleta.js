27007ebf2501649ed26dfa0ca815098be88dcc15
sergioTrmx <80510599+sergioTrmx@users.noreply.github.com>
2021-09-17 23:33:02 -0600

    Add files via upload

--- src/js/helproleta.js
+++ src/js/helproleta.js
@@ -1,10 +1,9 @@
-const helproleta = `Roleta russa consiste em escolhe um membro aleatório do grupo e de acordo com a dificuldade, o membro escolhido sofrerá as consequências.
-É bem fácil jogar, basta apenas dar o comando que começa em automático. Olhe abaixo as consequências de cada dificuldade
+const helproleta = `La ruleta rusa consiste en elegir un miembro del grupo al azar y según la dificultad, el miembro elegido sufrirá las consecuencias. Es muy fácil de jugar, solo da el comando que comienza en automático. Mira a continuación las consecuencias de cada dificultad
 
-roletarussapac - não faz nada, apenas diversão
-roletarussapac - bane o membro escolhido
-roletarussamed - bane e bloqueia o membro escolhido
-roletarussahard - bane, bloqueia e trava o membro escolhido
+roletarussapac - no pasa nada, solo diversión
+roletarussapac - banea al miembro elegido 
+roletarussamed - banea y bloquea miembro elegido 
+roletarussahard - banea, bloquea y manda binarios al elegido
 
 Obs: os efeitos são reversíveis, para desbloquear o membro basta apenas dar unblock (número sem espaço, traço ou +)`
 
